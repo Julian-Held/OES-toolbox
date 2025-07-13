324d15a925f50c3675272b6d6f651592e7a5ebd9
Julian Held <julian.held@gmail.com>
2025-07-13 15:46:43 +0200

    Restored my python start script. Still usefull...

---
+++ OES-toolbox.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt6.QtWidgets import QSplashScreen, QApplication
+from PyQt6.QtGui import QPixmap
+from OES_toolbox.ui import resources # seems unused but is needed!
+import sys
+from OES_toolbox import main
+
+if __name__ == '__main__':
+    main()
